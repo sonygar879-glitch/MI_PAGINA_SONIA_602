7a1743b0531cc056ecdb1f1d40059451052c76bf
sonygar879-glitch <sonygar879@gmail.com>
2026-04-20 14:40:40 -0600

    holaa

--- script.js
+++ script.js
@@ -10,13 +10,3 @@
     }, 50);
 }
 
-
-function MostrarMensaje(){
-    let mensaje=document.getElementById("mensaje");
-    mensaje.innerHTML="Cuida y protege a los animales";
-    mensaje.style.display="block";
-
-    setTimeout(()=>{
-        mensaje.style.opacity="1";
-        mensaje.style.transform= "translateY(0)";
-    }, 50);
